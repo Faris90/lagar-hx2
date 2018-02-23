0dc091069df565b5aa506a8d17b8aac573dd3af7
( ͡° ͜ʖ ͡°) Fs unlimited <faris.soliman@yahoo.com>
2018-02-23 16:25:11 -0600

    Update GameServer.js

--- src/GameServer.js
+++ src/GameServer.js
@@ -393,8 +393,11 @@ GameServer.prototype.virusCheck = function() {
         }
     	
         // Spawn if no cells are colliding
-        var v = new Entity.Virus(this.getNextNodeId(), null, pos, this.config.virusStartMass);
-        this.addNode(v);
+             if (Math.floor(Math.random()*2+1 == 2) {
+	    var v = new Entity.Virus(this.getNextNodeId(), null, pos, this.config.virusStartMass);
+    } else {
+	    var v = new Entity.test(this.getNextNodeId(), null, pos, this.config.virusStartMass);
+	   this.addNode(v);
     }
 }
 
